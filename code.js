0a404bfa3eab25258736ddc255baf39df4fe5693
Oscar Santana <oscarsantana2014@gmail.com>
2021-06-02 21:03:58 -0500

    intro to testing excercises

--- code.js
+++ code.js
@@ -2,3 +2,23 @@
 function helloWorld() {
     return "Hello, World!";
 }
+function sayHello(name) {
+    return "Hello, Jane!";
+}
+
+function sayHello(name) {
+    if (name === undefined) {
+        return 'Hello, World!';
+    }
+    return "Hello, " + name + "!";
+
+    if (name === "Alex") {
+        return "Hello, Alex!"
+    }
+    if (name === "Pat") {
+        return "Hello, Pat!";
+    }
+    return "";
+}
+
+
--- tests.js
+++ tests.js
@@ -13,3 +13,30 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function() {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+
+
+    it('it should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    })
+
+    it('should return the string "Hello, Jane!" when sayHello("Jane") is executed', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+
+    it('should return the string "Hello, Alex!" when sayHello("Alex") is executed', function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+
+    it('should return the string "Hello, Pat!" when sayHello("Pat") is executed', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+
+    it('should return the string "Hello, World!" when sayHello() is executed', function () {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+});
